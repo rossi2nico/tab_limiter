6dc18eec2780953ccd63699cad54ac7f8c5ea257
Nico <rossi2nick@gmail.com>
2025-07-09 18:39:43 -0400

    ux

--- popup.js
+++ popup.js
@@ -16,13 +16,22 @@ document.addEventListener('DOMContentLoaded', async () => {
   // Save maxTabs on button click or Enter
   function saveMaxTabs() {
     const value = parseInt(maxTabsInput.value.trim(), 10);
-    if (!isNaN(value) && value > 0) {
+    if (!isNaN(value) && value > 0 && value != maxTabs) {
       chrome.storage.local.set({ maxTabs: value });
+      maxTabsInput.value = null;
+      chrome.notifications.create({
+        type: 'basic',
+        iconUrl: 'icon.png',
+        title: 'Successfully set new limit',
+        message: `New tab limit: ${value}.`,
+      });
     }
   }
 
   setButton.addEventListener('click', saveMaxTabs);
   maxTabsInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') saveMaxTabs();
+    if (e.key === 'Enter') {
+      saveMaxTabs();
+    }
   });
 });
